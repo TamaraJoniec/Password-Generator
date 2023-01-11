07e1a54e63832a4bfd0c31d9fbac4a6096b459c5
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-01-11 10:32:59 +0000

    trying to make password show, added password variable in generate password but still bugging

--- script.js
+++ script.js
@@ -115,10 +115,10 @@ function getRandom(arr) {
 
 var choices;
 var enter;
-var password = [];
+var password = "";
 
 function generatePassword() {
-
+  password = "";
   enter = parseInt(prompt("Choose between 10 and 64 characters to create your password."));
   if (!enter) {
     alert("This needs a value");
@@ -173,6 +173,7 @@ function generatePassword() {
   else if (confirmlowerCasedCharacters) {
     choices = letter;
   }
+  return password;
 }
 
 // Get references to the #generate element
@@ -182,9 +183,9 @@ var generateBtn = document.querySelector('#generate');
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
-
   passwordText.value = password;
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
