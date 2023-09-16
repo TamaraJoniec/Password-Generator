d8d74ddabbe1677d6d4c54c067deea4859589363
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-09-16 11:40:16 +0100

    styled pop ups, changed vars to const and let

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Array of special characters to be included in password
-var specialCharacters = [
+const specialCharacters = [
   '@',
   '%',
   '+',
@@ -26,10 +26,10 @@ var specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+const lowerCasedCharacters = [
   'a',
   'b',
   'c',
@@ -59,7 +59,7 @@ var lowerCasedCharacters = [
 ];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
+const upperCasedCharacters = [
   'A',
   'B',
   'C',
@@ -89,10 +89,10 @@ var upperCasedCharacters = [
 ];
 
 
-var confirmnumericCharacters;
-var confirmspecialCharacters;
-var confirmupperCasedCharacters;
-var confirmlowerCasedCharacters;
+let confirmnumericCharacters;
+let confirmspecialCharacters;
+let confirmupperCasedCharacters;
+let confirmlowerCasedCharacters;
 
 
 // Function to prompt user for password options
@@ -105,57 +105,96 @@ function getPasswordOptions() {
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-  var pickChoices = ""
+  let pickChoices = ""
 
-  for (var i = 0; i < enter; i++) {
+  for (let i = 0; i < enter; i++) {
     pickChoices += arr[Math.floor(Math.random() * arr.length)];
   }
   return (pickChoices)
 }
 // Function to generate password with user input
 
-var enter;
+let enter;
 
 function generatePassword() {
   let choices = [];
-  enter = parseInt(prompt("Choose between 10 and 64 characters to create your password."));
-  if (!enter) {
-    alert("This needs a value");
-  } else if (enter < 10 || enter > 64) {
-    enter = parseInt(prompt("Choose between 10 and 64"));
-  } else {
-    confirmnumericCharacters = confirm("Do you want numbers?");
-    confirmspecialCharacters = confirm("Do you want special characters?");
-    confirmupperCasedCharacters = confirm("Do you want capital letters?");
-    confirmlowerCasedCharacters = confirm("Do you want lowercase letters?");
-  };
 
-  if (!confirmspecialCharacters && !confirmnumericCharacters && !confirmupperCasedCharacters && !confirmlowerCasedCharacters) {
-    alert("Choose criteria");
-    return
-  }
-// simplified from else if to if statements with help of T.A
-  if (confirmupperCasedCharacters) {
-    choices = choices.concat(upperCasedCharacters);
-  }
-  if (confirmspecialCharacters) {
-    choices = choices.concat(specialCharacters);
-  }
-  if (confirmnumericCharacters) {
-    choices = choices.concat(numericCharacters)
-  }
-  if (confirmlowerCasedCharacters) {
-    choices = choices.concat(lowerCasedCharacters)
-  };
-  return getRandom(choices);
+  Swal.fire({
+      title: 'Enter Password Length',
+      input: 'text',
+      inputPlaceholder: 'Choose between 10 and 64 characters'
+  }).then((result) => {
+      if (result.isConfirmed) {
+          enter = parseInt(result.value);
+
+          Swal.fire({
+              title: 'Do you want numbers?',
+              showDenyButton: true,
+              confirmButtonText: 'Yes',
+              denyButtonText: 'No',
+          }).then((result) => {
+              confirmnumericCharacters = result.isConfirmed;
+
+              Swal.fire({
+                  title: 'Do you want special characters?',
+                  showDenyButton: true,
+                  confirmButtonText: 'Yes',
+                  denyButtonText: 'No',
+              }).then((result) => {
+                  confirmspecialCharacters = result.isConfirmed;
+
+                  Swal.fire({
+                      title: 'Do you want capital letters?',
+                      showDenyButton: true,
+                      confirmButtonText: 'Yes',
+                      denyButtonText: 'No',
+                  }).then((result) => {
+                      confirmupperCasedCharacters = result.isConfirmed;
+
+                      Swal.fire({
+                          title: 'Do you want lowercase letters?',
+                          showDenyButton: true,
+                          confirmButtonText: 'Yes',
+                          denyButtonText: 'No',
+                      }).then((result) => {
+                          confirmlowerCasedCharacters = result.isConfirmed;
+
+                          // After all questions
+                          if (!confirmspecialCharacters && !confirmnumericCharacters && !confirmupperCasedCharacters && !confirmlowerCasedCharacters) {
+                              alert("Choose criteria");
+                              return;
+                          }
+
+                          if (confirmupperCasedCharacters) {
+                              choices = choices.concat(upperCasedCharacters);
+                          }
+                          if (confirmspecialCharacters) {
+                              choices = choices.concat(specialCharacters);
+                          }
+                          if (confirmnumericCharacters) {
+                              choices = choices.concat(numericCharacters);
+                          }
+                          if (confirmlowerCasedCharacters) {
+                              choices = choices.concat(lowerCasedCharacters);
+                          }
+
+                          return getRandom(choices);
+                      }); // End of lowercase letters question
+                  }); // End of capital letters question
+              }); // End of special characters question
+          }); // End of numbers question
+      }
+  }); // End of password length question
 }
+
+
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+let generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  let password = generatePassword();
+  let passwordText = document.querySelector('#password');
   passwordText.textContent = password;
 }
 
